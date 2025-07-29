486778f56f6be85c32a98c3bebc408f58bf686f1
Dave Meloncelli <110876707+Dave-Meloncelli@users.noreply.github.com>
2025-07-29 15:38:19 +0800

    Add index_tsx_content constant

---
+++ data/file-content/index_tsx_content.ts
@@ -0,0 +1,13 @@
+// data/file-content/index_tsx_content.ts
+export const index_tsx_content = `
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+`;
